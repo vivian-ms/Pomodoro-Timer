2662dacc001dc7b702b867b92f565839475b7edb
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-01-30 18:35:06 -0800

    Created Timer component

---
+++ my_app/src/components/Timer.js
@@ -0,0 +1,13 @@
+const Timer = () => {
+
+  return (
+    <div id="timer">
+      <h2 id="timer-label">Session</h2>
+      <p id="time-left">25:00</p>
+      <button id="start_stop">Start/Pause</button>
+      <button id="reset">Reset</button>
+    </div>
+  );
+};
+
+export default Timer;
