d0ae9285aee95b511fa7ecf1a75d7c80cef3563f
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-01-31 00:47:08 -0800

    Created state for setInterval id and passed it to <Timer />

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -9,12 +9,13 @@ const DEFAULT_BREAK = 5 * 60;  // 5 min
 const App = () => {
   const [session, setSession] = useState(DEFAULT_SESSION);
   const [rest, setRest] = useState(DEFAULT_BREAK);
+  const [isRunning, setIsRunning] = useState(false);  // Timer not running
 
   return (
     <div>
       <h1>Pomodoro Timer</h1>
 
-      <Timer session={session} rest={rest} />
+      <Timer session={session} rest={rest} isRunning={isRunning} setIsRunning={setIsRunning} />
     </div>
   );
 };  // End <App />
