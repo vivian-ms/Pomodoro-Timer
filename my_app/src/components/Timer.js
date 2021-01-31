ec1c38f4e037f877e428d7c437466e91f80a6474
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-01-31 00:17:32 -0800

    Created function to display time in mm:ss format

--- my_app/src/components/Timer.js
+++ my_app/src/components/Timer.js
@@ -1,5 +1,12 @@
-const Timer = () => {
+// Function to display time in mm:ss format
+const formatTime = time => {
+  let sec = (time % 60).toString().padStart(2, '0');
+  let min = Math.floor(time / 60).toString().padStart(2, '0');
+  return `${min}:${sec}`;
+};  // End formatTime()
 
+
+const Timer = ({ session, rest }) => {
   return (
     <div id="timer">
       <h2 id="timer-label">Session</h2>
@@ -8,6 +15,6 @@ const Timer = () => {
       <button id="reset">Reset</button>
     </div>
   );
-};
+};  // End <Timer />
 
 export default Timer;
