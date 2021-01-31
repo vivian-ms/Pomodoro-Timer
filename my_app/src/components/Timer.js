87ae8bca80504deb5fccccb0f25b994bcb9296f5
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-01-31 00:33:35 -0800

    Created state for time in mm:ss format. Changed text in #time-left to display state

--- my_app/src/components/Timer.js
+++ my_app/src/components/Timer.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 // Function to display time in mm:ss format
 const formatTime = time => {
   let sec = (time % 60).toString().padStart(2, '0');
@@ -7,10 +9,12 @@ const formatTime = time => {
 
 
 const Timer = ({ session, rest }) => {
+  const [formattedTime, setFormattedTime] = useState(formatTime(session));
+
   return (
     <div id="timer">
       <h2 id="timer-label">Session</h2>
-      <p id="time-left">25:00</p>
+      <p id="time-left">{formattedTime}</p>
       <button id="start_stop">Start/Pause</button>
       <button id="reset">Reset</button>
     </div>
