b2393a1686dcafde4a54b01524931822af1447ce
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-01-31 00:15:37 -0800

    Created state for session and break time and passed them as props to <Timer />

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import Timer from './components/Timer';
 
+
+const DEFAULT_SESSION = 25 * 60;  // 25 min
+const DEFAULT_BREAK = 5 * 60;  // 5 min
+
+
 const App = () => {
+  const [session, setSession] = useState(DEFAULT_SESSION);
+  const [rest, setRest] = useState(DEFAULT_BREAK);
+
   return (
     <div>
       <h1>Pomodoro Timer</h1>
 
-      <Timer />
+      <Timer session={session} rest={rest} />
     </div>
   );
-};
+};  // End <App />
+
 
 export default App;
