a3beabd76d676144d552c90ada30c545de2b5e7d
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-01-31 00:48:19 -0800

    Created function that starts/pause timer when Start/Pause button is clicked.

--- my_app/src/components/Timer.js
+++ my_app/src/components/Timer.js
@@ -8,14 +8,35 @@ const formatTime = time => {
 };  // End formatTime()
 
 
-const Timer = ({ session, rest }) => {
+const Timer = ({ session, rest, isRunning, setIsRunning }) => {
   const [formattedTime, setFormattedTime] = useState(formatTime(session));
+  const [timeLeft, setTimeLeft] = useState(session);
+
+  const start_stop = () => {
+      // Start timer
+    if (!isRunning) {
+      setIsRunning(setInterval(() => {
+        setTimeLeft(prevTimeLeft => {
+          if (prevTimeLeft > 0) {
+            return prevTimeLeft - 1;
+          } else {
+            return prevTimeLeft;
+          }
+        });
+      }, 1000));
+
+      // Pause timer
+    } else {
+      clearInterval(isRunning);
+      setIsRunning(false);
+    }
+  };  // End start_stop()
 
   return (
     <div id="timer">
       <h2 id="timer-label">Session</h2>
       <p id="time-left">{formattedTime}</p>
-      <button id="start_stop">Start/Pause</button>
+      <button id="start_stop" onClick={start_stop}>Start/Pause</button>
       <button id="reset">Reset</button>
     </div>
   );
