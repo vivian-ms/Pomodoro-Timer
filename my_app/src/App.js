d210abe0ab41a9ba1deddad7646df90ec1b43979
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-01-31 19:33:26 -0800

    Added two <Control />, one for session and one for break

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Timer from './components/Timer';
+import Control from './components/Control';
 
 
 const DEFAULT_SESSION = 25 * 60;  // 25 min
@@ -16,6 +17,9 @@ const App = () => {
       <h1>Pomodoro Timer</h1>
 
       <Timer DEFAULT_SESSION={DEFAULT_SESSION} DEFAULT_BREAK={DEFAULT_BREAK} session={session} rest={rest} setSession={setSession} setRest={setRest} isRunning={isRunning} setIsRunning={setIsRunning} />
+
+      <Control length={session} setLength={setSession} type="session" />
+      <Control length={rest} setLength={setRest} type="break" />
     </div>
   );
 };  // End <App />
