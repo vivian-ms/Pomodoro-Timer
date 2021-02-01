a41bc0cd061005e77cf1345d5c7ee3f1b6eb68b7
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-01-31 23:45:16 -0800

    Added code that disables buttons if timer is running or time is <1 min or >60 min

--- my_app/src/components/Control.js
+++ my_app/src/components/Control.js
@@ -1,14 +1,12 @@
-const Control = ({ length, setLength, type }) => {
+const Control = ({ length, setLength, type, isRunning }) => {
   const increment = () => {
-    if (length < 3600) {
-      setLength(prevLength => prevLength + 60);
-    }
+      // Increment 1 min (60 sec)
+    setLength(prevLength => prevLength + 60);
   };
 
   const decrement = () => {
-    if (length > 60) {
-      setLength(prevLength => prevLength - 60);
-    }
+      // Decrement 1 min (60 sec)
+    setLength(prevLength => prevLength - 60);
   };
 
   return (
@@ -19,8 +17,13 @@ const Control = ({ length, setLength, type }) => {
       <p id="length-container">
         <span id={`${type}-length`}>{length / 60}</span> min
       </p>
-      <button id={`${type}-decrement`} onClick={decrement}>-</button>
-      <button id={`${type}-increment`} onClick={increment}>+</button>
+
+      {/* Disable buttons if timer is running */}
+      {/* Disable decrement if length < 1 min */}
+      <button id={`${type}-decrement`} onClick={decrement} disabled={(!isRunning && length > 60) ? false : true}>-</button>
+
+      {/* Disable increment if length > 60 min */}
+      <button id={`${type}-increment`} onClick={increment} disabled={(!isRunning && length < 3600) ? false : true}>+</button>
     </div>
   );
 };  // End <Control />
