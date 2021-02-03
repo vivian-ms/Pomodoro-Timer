2503d86048401a7fc9a8eb350e8ddc2792b27ef1
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-02-02 23:44:38 -0800

    Added footer

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -16,12 +16,14 @@ const App = () => {
     <div className="container-fluid p-3">
       <h1 className="text-center mb-4">Pomodoro Timer</h1>
 
-      <Timer DEFAULT_SESSION={DEFAULT_SESSION} DEFAULT_BREAK={DEFAULT_BREAK} session={session} rest={rest} setSession={setSession} setRest={setRest} isRunning={isRunning} setIsRunning={setIsRunning} />
+      <Timer DEFAULT_SESSION={DEFAULT_SESSION} DEFAULT_BREAK={DEFAULT_BREAK} session={session} rest={rest} isRunning={isRunning} setSession={setSession} setRest={setRest} setIsRunning={setIsRunning} />
 
       <div id="controls-container" className="d-flex justify-content-center flex-wrap">
         <Control length={session} setLength={setSession} type="session" isRunning={isRunning} />
         <Control length={rest} setLength={setRest} type="break" isRunning={isRunning} />
       </div>
+
+      <footer className="small">Created by Vivian S for <a href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-25--5-clock" target="_blank" rel="noopener noreferrer">freeCodeCamp</a></footer>
     </div>
   );
 };  // End <App />
